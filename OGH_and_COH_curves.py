5e013d540d3298d21dd9d603fdd7334785b4fe4b
ViliamVadocz <viliam.vadocz@gmail.com>
2019-07-09 23:02:26 +0200

    implemented M4 and probably broke some stuff

--- OGH_and_COH_curves.py
+++ OGH_and_COH_curves.py
@@ -5,19 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def counterclockwise_angle(v):
+    """Returns the counterclockwise angle of a vector measured from the positive x-axis."""
     angle = np.arctan2(v[1],v[0])
-    return angle if angle >= 0 else 2*np.pi + angle
+    return angle[0] if angle >= 0 else 2*np.pi + angle[0]
 
 def angle_between_vectors(a, b):
-    """Returns the angle in radians between vectors a and b"""
+    """Returns the counterclockwise angle in radians between vectors a and b, measured from a to b."""
     angle_a = counterclockwise_angle(a)
     angle_b = counterclockwise_angle(b)
-    angle =  angle_a - angle_b if angle_a >= angle_b else angle_b - angle_a
-    return angle if angle <= np.pi else 2*np.pi - angle
+    angle = angle_b - angle_a
+    return angle if angle >= 0 else 2*np.pi + angle
 
 def vector_from_angle(angle : float, magnitude : float = 1):
     """Returns a vector with a given angle and length"""
-    return magnitude * np.array([np.cos(angle),np.sin(angle)])
+    return magnitude * np.array([[np.cos(angle)],[np.sin(angle)]])
 
 def Q(p0, p1, v0, v1, t0, t1, t):
     """Basic Hermite curve."""
@@ -65,6 +66,7 @@ def COH(p0, p1, v0, v1, t0, t1, t):
     # M1: two-segment COH curve.
     elif (0 <= theta <= np.pi/6) and (np.pi/3 <= phi <= 2*np.pi/3):
         print("M1")
+        plt.title("M1")
 
         pT = p1 - vector_from_angle(phi/2 + alpha, np.linalg.norm(p1-p0)/3)
         beta : float = angle_between_vectors(pT-p0,p1-pT)
@@ -76,6 +78,7 @@ def COH(p0, p1, v0, v1, t0, t1, t):
     # M2: two-segment COH curve.
     elif ((0 <= theta <= np.pi/3) and (np.pi <= phi <= 5*np.pi/3)) or ((np.pi/3 <= theta <= 2*np.pi/3) and (4*np.pi/3 <= phi <= 5*np.pi/3)):
         print("M2")
+        plt.title("M2")
 
         A : float = np.pi/18 if theta < np.pi/9 else theta/2
         B : float = 2*np.pi - phi - (2*np.pi-phi+theta-A)/3
@@ -92,6 +95,7 @@ def COH(p0, p1, v0, v1, t0, t1, t):
     # M3: three-segment COH curve.
     elif (0 <= theta <= np.pi/3) and (np.pi/3 <= phi <= np.pi):
         print("M3")
+        plt.title("M3")
 
         beta : float = phi/3
         a1 : float = (theta - beta)/2 - np.pi/18
@@ -112,12 +116,26 @@ def COH(p0, p1, v0, v1, t0, t1, t):
         vT0 = vector_from_angle(alpha + a2)
 
         L : float = np.pi - a3 - a5
-        l = np.linalg.norm(p1-pT0)
-        K = angle_between_vectors(p0-p1,p1-pT0)
-        k = l*np.sin(a3+K) / np.sin(L)
+        l : float = np.linalg.norm(p1-pT0)
+        K : float = angle_between_vectors(p0-p1,pT0-p1)
+        k : float = l*np.sin(a3+K) / np.sin(L)
 
         pT1 = p1 - vector_from_angle(a5,k)
         vT1 = vector_from_angle(alpha + a4)
+
+        return np.concatenate([OGH(p0,pT0,v0,vT0,t0,t1,t),OGH(pT0,pT1,vT0,vT1,t0,t1,t),OGH(pT1,p1,vT1,v1,t0,t1,t)],axis=1)
+
+    elif (np.pi/3 <= theta <= 2*np.pi/3) and (0 <= phi <= 2*np.pi/3):
+        print("M4")
+        plt.title("M4")
+
+        l : float = np.linalg.norm(p1-p0)
+
+        pT0 = p0 + vector_from_angle(alpha + theta/2, l/3)
+        pT1 = p1 - vector_from_angle(alpha + phi/2, l/6)
+
+        vT0 = vector_from_angle(alpha + theta/2 - angle_between_vectors(pT1-pT0, pT0-p0)/2)
+        vT1 = vector_from_angle(alpha + phi/2 - angle_between_vectors(pT1-pT0, p1-pT1)/2)
 
         return np.concatenate([OGH(p0,pT0,v0,vT0,t0,t1,t),OGH(pT0,pT1,vT0,vT1,t0,t1,t),OGH(pT1,p1,vT1,v1,t0,t1,t)],axis=1)
 
@@ -126,10 +144,10 @@ def COH(p0, p1, v0, v1, t0, t1, t):
 
 # Parameters.
 p0 = np.array([[-5], [0]])
-v0 = np.array([[1], [1]])
+v0 = np.array([[1], [4]])
 
-p1 = np.array([[5], [3]])
-v1 = np.array([[-5], [-1]])
+p1 = np.array([[5], [0]])
+v1 = np.array([[1], [1]])
 
 t0 = 0
 t1 = 1
