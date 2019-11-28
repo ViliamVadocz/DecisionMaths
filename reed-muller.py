a2c289abd9677f7855e10ee517f72864a9fbbc07
ViliamVadocz <viliam.vadocz@gmail.com>
2019-11-28 14:59:53 +0100

    Translating image to Reed Muller code

--- reed-muller.py
+++ reed-muller.py
@@ -46,17 +46,71 @@ def M2(n):
     """
     return  (H(n) + 1) / 2
 
-# Create image.
-img = Image.fromarray(M2(5).astype('uint8') * 255)
-img.save('reed-muller.png')
+def reed_muller_img(n):
+    """
+    Create image of Reed Muller code.
+    """
+    img = Image.fromarray(M2(n).astype('uint8') * 255)
+    img.save('reed-muller.png')
+    img.show()
+
+def hamming_dis(a, b):
+    """
+    Find the Hamming distance between a and b.
+    """
+    count = 0
+    for i in range(len(a)):
+        if a[i] != a[i]:
+            count += 1
+    return count
+
+### ### ### ### ### ### ### ### ### ### ### ### ###
+
+# Generate Reed-Muller code for 256 bit data.
+reed_muller = M(8)
+
+# Message to send (example img)
+data = np.asarray(Image.open('test_img.png'))
+
+# Convert to Reed-Muller code.
+origin_bits = np.empty((data.shape[0]*data.shape[1]*data.shape[2], 256), dtype='uint8')
+i = 0
+for row in data:
+    for pixel in row:
+        for colour in pixel:
+            origin_bits[i] = reed_muller[colour]
+            i += 1
+
+# Corrupt per pixel code.
+corrupt_bits = origin_bits.copy()
+max_mistakes = 100
+for code in corrupt_bits:
+    # Randomise number of mistakes.
+    for i in range( int(np.random.rand(1)*(max_mistakes+1)) ):
+        # Flip a bit at a random position.
+        position = int(np.random.rand(1)*len(code))
+        code[position] += 1
+        code[position] %= 2
+
+# Reconstruct message.
+# FIXME
+reconstruction = np.empty_like(corrupt_bits.shape[0])
+for i, code in enumerate(corrupt_bits):
+
+    # Find closest.
+    closest = 0
+    lowest_dis = 256
+    for ii, row in enumerate(reed_muller):
+        dis = hamming_dis(code, row)
+        if dis < lowest_dis:
+            closest = ii
+            lowest_dis = dis
+
+    # Replace with closest.
+    reconstruction[i] = closest
+
+# Create image
+reconstruction = reconstruction.reshape(data.shape)
+img = Image.fromarray(reconstruction)
+# img.save('test_img_reconstructed.png')
 img.show()
-
-# Generate code.
-reed_muller = M(5)
-
-# TODO
-# Generate message to send
-# Corrupt message
-# Reconstruct message
-# Decode message
-# Show results   
