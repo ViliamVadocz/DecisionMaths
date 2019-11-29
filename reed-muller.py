2fe7386a9fc3a4b507a9cee3225b7fcffafe73de
ViliamVadocz <viliam.vadocz@gmail.com>
2019-11-29 20:56:24 +0100

    Potentially working?

--- reed-muller.py
+++ reed-muller.py
@@ -6,7 +6,7 @@ def cartesian_product(X, Y):
     x_n = X.shape[1]
     y_m = Y.shape[0]
     y_n = Y.shape[1]
-    arr = np.empty((x_m * y_m, x_n * y_n))
+    arr = np.empty((x_m * y_m, x_n * y_n), dtype='int8')
     for row in range(x_m):
         for col in range(x_n): 
             if X[row,col] == 1:
@@ -21,7 +21,7 @@ def cartesian_product(X, Y):
 H1 = np.array([
     [ 1,  1],
     [ 1, -1]
-])
+], dtype='int8')
 
 def H(n):
     """
@@ -36,15 +36,20 @@ def M(n):
         -1 as 1,
         1 as 0.
     """
-    return  np.abs((H(n) - 1) / 2)
+    matrix = H(n)
+    matrix[matrix == 1] = 0
+    matrix[matrix == -1] = 1
+    return  matrix
 
 def M2(n):
     """
     Generate Hadamard matrix but with
         -1 as 0,
-        1 as 1.
+        1 as 1 (leaves the same).
     """
-    return  (H(n) + 1) / 2
+    matrix = H(n)
+    matrix[matrix == -1] = 0
+    return  matrix
 
 def reed_muller_img(n):
     """
@@ -60,7 +65,9 @@ def hamming_dis(a, b):
     """
     count = 0
     for i in range(len(a)):
-        if a[i] != a[i]:
+        # print(i, a[i], b[i])
+        if a[i] != b[i]:
+            # print('hey')
             count += 1
     return count
 
@@ -72,7 +79,7 @@ reed_muller = M(8)
 # Message to send (example img)
 data = np.asarray(Image.open('test_img.png'))
 
-# Convert to Reed-Muller code.
+# Convert pixels to Reed-Muller code.
 origin_bits = np.empty((data.shape[0]*data.shape[1]*data.shape[2], 256), dtype='uint8')
 i = 0
 for row in data:
@@ -83,25 +90,29 @@ for row in data:
 
 # Corrupt per pixel code.
 corrupt_bits = origin_bits.copy()
-max_mistakes = 100
+max_mistakes = 150 # Parameter
+total_mistakes = 0
 for code in corrupt_bits:
     # Randomise number of mistakes.
-    for i in range( int(np.random.rand(1)*(max_mistakes+1)) ):
+    num_mistakes = int(np.random.rand(1) * (max_mistakes+1))
+    positions = np.random.rand(num_mistakes) * len(code)
+    for i in range(num_mistakes):
         # Flip a bit at a random position.
-        position = int(np.random.rand(1)*len(code))
+        position = int(positions[i])
         code[position] += 1
         code[position] %= 2
 
-# Reconstruct message.
-# FIXME
-reconstruction = np.empty_like(corrupt_bits.shape[0])
-for i, code in enumerate(corrupt_bits):
+    total_mistakes += num_mistakes
 
+# Reconstruct message.
+reconstruction = np.empty(corrupt_bits.shape[0], dtype='int8')
+for i, code in enumerate(corrupt_bits):
     # Find closest.
     closest = 0
     lowest_dis = 256
-    for ii, row in enumerate(reed_muller):
+    for ii, row in enumerate(reed_muller):   
         dis = hamming_dis(code, row)
+
         if dis < lowest_dis:
             closest = ii
             lowest_dis = dis
@@ -109,8 +120,17 @@ for i, code in enumerate(corrupt_bits):
     # Replace with closest.
     reconstruction[i] = closest
 
+test = np.empty_like(data)
+j = 0
+for i, row in enumerate(test):
+    for ii, pixel in enumerate(row):
+        for iii, colour in enumerate(pixel):
+            test[i, ii, iii] = reconstruction[j]
+            j += 1
+
 # Create image
-reconstruction = reconstruction.reshape(data.shape)
-img = Image.fromarray(reconstruction)
+# reconstruction = reconstruction.reshape(data.shape)
+img = Image.fromarray(test)
 # img.save('test_img_reconstructed.png')
 img.show()
+print(f'Total mistakes: {total_mistakes}')
