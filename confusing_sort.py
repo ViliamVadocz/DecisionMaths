02afc0b3382c2b31148d5189639abd478ce408c0
ViliamVadocz <viliam.vadocz@gmail.com>
2020-02-05 08:15:04 +0100

    Don't make an unnecessarily large track list

--- confusing_sort.py
+++ confusing_sort.py
@@ -1,20 +1,27 @@
 # What is the time complexity of this sort?
-
+ 
 # List contains positive integers (can include 0).
-my_list = [6,4,12,63,11,0,25,11,29,5,46,1]
+my_list = [6, 4, 12, 63, 11, 25, 11, 29, 5, 46, 32, 34, 7]
 
-# Find largest in list.
+# Find largest and smallest in list to define range.
+smallest = my_list[0]
 largest = my_list[0]
 for element in my_list:
     if element > largest:
         largest = element
+    elif element < smallest:
+        smallest = element
 
-# Make new list of length 'largest + 1'.
-track_list = [0] * (largest + 1)
+# Make new list to track which numbers appear.
+track_list = [0] * (largest - smallest + 1)
 
-# Add 1 to index of element's value.
+# Add 1 to index of element's value - smallest.
 for element in my_list:
-    track_list[element] += 1
+    track_list[element - smallest] += 1
+
+# Print tracked list.
+for i, ii in enumerate(track_list):
+    print(ii + smallest, i)
 
 # Init sorted list.
 sorted_list = []
@@ -27,7 +34,7 @@ for i, value in enumerate(track_list):
     
     # For non-zeros, add the index to the sorted list 'value' times.
     for _ in range(value):
-        sorted_list.append(i)
+        sorted_list.append(i + smallest)
 
 # Print sorted list.
 print(sorted_list)
